88bb48d541561613f89c45c701e29aca352043bd
César Córdoba <cordobacesar29@gmail.com>
2021-06-16 15:57:02 -0300

    conectando mongo atlas con express a travez de mongoose

--- Backend/index.js
+++ Backend/index.js
@@ -1,1 +1,16 @@
-console.log('hola mundo')
+require('dotenv').config(); // obtengo los datos del archivo .env
+const express = require('express'); 
+const conection = require('./db'); // hago la conexion con la base de datos
+conection(); // lo primero que hago es llamar a la base de datos.
+const app = express();
+const port = 3000;
+const rutas  = require('./rutas');
+
+app.use(express.json());
+app.use(express.urlencoded( {extended: true}));
+
+rutas(app);
+  
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+});
